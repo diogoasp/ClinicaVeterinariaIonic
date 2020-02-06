a47fa6abb006042622a872a02799aa3d06d9ab14
Rafael <faelzapata2011@gmail.com>
2020-02-06 16:35:07 -0300

    List of "consultas" working

--- src/app/consultation/consultation.page.ts
+++ src/app/consultation/consultation.page.ts
@@ -1,12 +1,38 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Observable } from 'rxjs';
+import { Router } from '@angular/router';
+import { ApiAnimalService } from 'src/app/service/api-animais.service';
+import { Consulta } from 'src/app/models/Consulta';
+import { ApiConsultasService } from 'src/app/service/api-consultas.service';
+import { Animal } from '../models/Animal';
 
 @Component({
   selector: 'app-consultation',
   templateUrl: 'consultation.page.html',
   styleUrls: ['consultation.page.scss']
 })
-export class ConsultationPage {
+export class ConsultationPage implements OnInit{
 
-  constructor() {}
+  consultas: Observable<any>
+  consulta: Consulta;
+  animal: Animal;
+  animais: Observable<any>;
 
+  constructor(private router: Router, private apiConsulta: ApiConsultasService, private apiAnimal: ApiAnimalService) {
+    this.getAllConsultas();
+  }
+
+  ngOnInit(){}
+
+  async getAllConsultas() {
+    this.consultas = this.apiConsulta.getAll();
+  }
+
+  showDetailsConsulta(id: number){
+    this.apiConsulta.get(id);
+  }
+
+  showDetailsAnimal(id: number) {
+    this.animais = this.apiAnimal.get(id);
+  }
 }
